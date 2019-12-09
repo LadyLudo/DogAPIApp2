7232efeb4474657771377d7e66b86270a4d4415f
Catherine Davis <catherinevtdavis@gmail.com>
2019-12-08 18:17:38 -0700

    Added picture display

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ function getDogImages() {
     let number = $("input[type='text']").val();
     fetch(`https://dog.ceo/api/breeds/image/random/${number}`)
         .then(response => response.json())
-        .then(responseJson => console.log(responseJson.message))
+        .then (function (responseJson){
+            displayResults(responseJson);
+            console.log(responseJson)})
         .catch(error => alert('Something went wrong. Try again later.'));
 }
 function watchForm() {
@@ -12,6 +14,14 @@ function watchForm() {
         event.preventDefault();
         getDogImages();
     });
+}
+function displayResults(responseJson) {
+    $('.images').empty();
+    for (let i=0; i < responseJson.message.length; i++) {
+        console.log('picture loaded');
+        $('.images').append(`<img src="${responseJson.message[i]}" class="results-img"><br>`);
+    }
+
 }
 $(function() {
     console.log('App loaded! Waiting for submit!')
